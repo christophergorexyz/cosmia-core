e4aa5450e4e77fb33b7ca576d105b651323e3ba2
Christopher J Gore <christopher.gore@razorfishhealth.com>
2017-02-03 15:40:14 -0500

    collections and indexes of collections now published

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ const COSMIA_COLLECTION_PREFIX = 'cosmia-collection-';
 const PACKAGE_NAME = chalk.blue('cosmia-core');
 
 const ERROR_MESSAGE_COSMIA_CUSTOM_CHILD = 'cosmia custom elements may only have a single text child.';
-const ERORR_MESSAGE_COSMIA_CUSTOM_ELEMENT = 'Only one of a given type of cosmia custom element is allowed per page.';
+const ERROR_MESSAGE_COSMIA_CUSTOM_ELEMENT = 'Only one of a given type of cosmia custom element is allowed per page.';
 
 var siteData = {};
 var handlebarsLayouts = {};
@@ -63,7 +63,7 @@ function _extractCustomPageElement(page, attribute, process) {
         dom, true);
 
     if (dataElements.length > 1) {
-        throw ERORR_MESSAGE_COSMIA_CUSTOM_ELEMENT;
+        throw ERROR_MESSAGE_COSMIA_CUSTOM_ELEMENT;
     }
 
     if (dataElements.length) {
@@ -90,114 +90,142 @@ function _extractCustomPageElement(page, attribute, process) {
 }
 
 function _registerDataFile(name, content, dirName) {
-    var splitPath = name.replace(dataDir + '/', '').split('/');
-    var treeNode = siteData;
-    var objectName = '';
-    var dataObject = JSON.parse(content);
-    while (splitPath.length) {
-        objectName = splitPath.shift();
-        treeNode[objectName] = splitPath.length ? Object.assign({}, treeNode[objectName]) : dataObject;
-        treeNode = treeNode[objectName];
-    }
+    return Promise.resolve().then(() => {
+        var splitPath = name.replace(dataDir + '/', '').split('/');
+        var treeNode = siteData;
+        var objectName = '';
+        var dataObject = JSON.parse(content);
+        while (splitPath.length) {
+            objectName = splitPath.shift();
+            treeNode[objectName] = splitPath.length ? Object.assign({}, treeNode[objectName]) : dataObject;
+            treeNode = treeNode[objectName];
+        }
+    });
 }
 
 function _registerPartialFile(name, content, dirName) {
-    handlebars.registerPartial(path.basename(name), content);
+    return Promise.resolve().then(() => {
+        handlebars.registerPartial(path.basename(name), content);
+    });
 }
 
 function _registerLayoutFile(name, content, dirName) {
-    var layout = {
-        content: content,
-        path: name
-    };
-    layout = _extractCustomPageElement(layout, COSMIA_TEMPLATE_DATA, (e) => JSON.parse(DomUtils.getInnerHTML(e)));
-    layout.compile = handlebars.compile(layout.content);
-    handlebarsLayouts[path.basename(name)] = layout;
+    return Promise.resolve().then(() => {
+        var layout = {
+            content: content,
+            path: name
+        };
+        layout = _extractCustomPageElement(layout, COSMIA_TEMPLATE_DATA, (e) => JSON.parse(DomUtils.getInnerHTML(e)));
+        layout.compile = handlebars.compile(layout.content);
+        handlebarsLayouts[path.basename(name)] = layout;
+    });
 }
 
 function _registerHelperFile(name, content, dirName) {
-    handlebars.registerHelper(require(path.resolve(name)));
+    return Promise.resolve().then(() => {
+        handlebars.registerHelper(require(path.resolve(name)));
+    });
 }
 
 function _processPage(name, content, dirName) {
-    var page = {
-        path: name,
-        content: content
-    };
-    page = _extractCustomPageElement(page, COSMIA_DATA, (e) => JSON.parse(DomUtils.getInnerHTML(e)));
-    page = _extractCustomPageElement(page, COSMIA_SCRIPT, (e) => DomUtils.getOuterHTML(e));
-    var keyName = path.join('.', name.replace(dirName, ''));
-    pageData[keyName] = page;
+    return Promise.resolve().then(() => {
+        var page = {
+            path: name,
+            content: content
+        };
+        page = _extractCustomPageElement(page, COSMIA_DATA, (e) => JSON.parse(DomUtils.getInnerHTML(e)));
+        page = _extractCustomPageElement(page, COSMIA_SCRIPT, (e) => DomUtils.getOuterHTML(e));
+        var keyName = path.join('.', name.replace(dirName, ''));
+        pageData[keyName] = page;
+    });
 }
 
 //read all the files of a given type in a directory and execute a process on their content
 //the processor takes the form function (name, content, dirName, key){ ... }
 function _processDirectory(dirName, extension, processor, key) {
     return new Promise((resolve, reject) => {
+        var processors = [];
         recursiveReaddir(dirName, (err, files) => {
             if (err) {
                 reject(err);
                 return;
             }
+
             files.forEach((filename) => {
                 if (path.extname(filename) === extension) {
 
                     var nameWithoutExtension = path.resolve(path.dirname(filename), path.basename(filename, extension));
                     var fileContent = fs.readFileSync(path.resolve(dirName, filename), 'utf8');
 
-                    new Promise((resolve, reject) => {
-                        try {
-                            processor(nameWithoutExtension, fileContent, dirName, key);
-                        } catch (err) {
-                            reject(err);
-                            return;
-                        }
-                    }).catch((err) => {
+                    try {
+                        processors.push(processor(nameWithoutExtension, fileContent, dirName, key));
+                    } catch (err) {
                         reject(err);
-                    });
+                        return;
+                    }
                 }
             });
-            return resolve();
+            resolve(Promise.all(processors));
         });
-    }).catch((err) => {
-        console.error(err);
     });
 }
 
 //handle collection content
 function _processCollectionFile(name, content, dirName, key) {
-    var data = collectionData[key];
-    name = name.replace(dirName, '');
-    name = (data['single-path'] ? data['single-path'] : data['index-path']) + name;
-    var page = {
-        path: name,
-        content: content
-    };
+    return new Promise(function (resolve, reject) {
+        var data = collectionData[key];
+        name = name.replace(dirName, path.join(pagesDir, (data['single-path'] ? data['single-path'] : data['index-path'])));
+        //name = (data['single-path'] ? data['single-path'] : data['index-path']) + name;
+        var page = {
+            path: name,
+            content: content
+        };
 
-    var keyName = path.join('.', name);
-    for (var field of keys(data['content-fields'])) {
-        page = _extractCustomPageElement(page, `${COSMIA_COLLECTION_PREFIX}${field}`, (e) => DomUtils.getInnerHTML(e));
+        var cosmiaData = {};
+
+        var keyName = path.join('.', name);
+        for (var field of keys(data['content-fields'])) {
+            var fieldName = `${COSMIA_COLLECTION_PREFIX}${field}`;
+            page = _extractCustomPageElement(page, fieldName, (e) => DomUtils.getInnerHTML(e));
+
+            cosmiaData[field] = page[fieldName];
+            //page[fieldName] = page[fieldName];
+        }
+
         page = _extractCustomPageElement(page, COSMIA_COLLECTION_DATA, (e) => JSON.parse(DomUtils.getInnerHTML(e)));
-    }
 
-    page[COSMIA_DATA] = Object.assign({}, page[`${COSMIA_COLLECTION_PREFIX}${field}`], page[COSMIA_COLLECTION_DATA]);
-    page[COSMIA_DATA]['layout'] = collectionData[key]['single-layout'];
-    pageData[keyName] = page;
-    collectionData[key]['page'] = page;
-    console.log('processed collection file...');
+        page[COSMIA_DATA] = Object.assign({}, cosmiaData, page[COSMIA_COLLECTION_DATA]);
+        page[COSMIA_DATA]['layout'] = collectionData[key]['single-layout'];
+        pageData[keyName] = page;
+        pageData[key][COSMIA_DATA]['collection-items'].push(page[COSMIA_DATA]);
+
+        return resolve();
+    });
 }
 
 //handle collection meta data
 function _processCollectionData(name, content, dirName) {
-    console.log('processing collection data..');
     var collection = JSON.parse(content);
-    var keyName = path.join('.', name.replace(dirName, ''));
+    var keyName = path.join(pagesDir, collection['index-path']); //name.replace(dirName, pagesDir);
     collectionData[keyName] = collection;
     var collectionSourceDir = path.resolve(srcDir, collectionData[keyName]['source']);
-    _processDirectory(collectionSourceDir, EXTENSION_MD, _processCollectionFile, keyName)
-        .catch((err) => {
-            console.log('err');
-        });
+
+    /*TODO SCRATCH*/
+    //TODO: generate index and archives here
+    var indexPath = path.join(pagesDir, collection['index-path'], 'index');
+    var page = {
+        path: indexPath,
+        content: ''
+    };
+
+    page[COSMIA_DATA] = {};
+    page[COSMIA_DATA]['layout'] = collection['index-layout'] ? collection['index-layout'] : 'default';
+    page[COSMIA_DATA]['collection-items'] = [];
+    /*END TODO SCRATCH*/
+
+    pageData[keyName] = page;
+
+    return _processDirectory(collectionSourceDir, EXTENSION_MD, _processCollectionFile, keyName);
 }
 
 function _registerAppComponents() {
@@ -205,20 +233,15 @@ function _registerAppComponents() {
     handlebars.registerHelper(require('handlebars-helpers')());
 
     //register custom layouts, partials, data, and helpers
-    return Promise.all(
+    return Promise.all([
         _processDirectory(partialsDir, EXTENSION_HBS, _registerPartialFile),
         _processDirectory(dataDir, EXTENSION_JSON, _registerDataFile),
         _processDirectory(layoutsDir, EXTENSION_HBS, _registerLayoutFile),
         _processDirectory(helpersDir, EXTENSION_JS, _registerHelperFile)
-    ).then(() => {
-        console.log(`${PACKAGE_NAME}: Components registered`);
-    }).catch((err) => {
-        console.error(err);
-    });
+    ]);
 }
 
 function _compilePage(page, customData = {}, silent = false) {
-    console.log('compile page');
     //TODO: make sure cosmia custom elements are set in a single pass.
     //as it stands, we'll need two new lines for each additional custom
     //element, but it could be reduced to one new line
@@ -249,7 +272,7 @@ function _compilePage(page, customData = {}, silent = false) {
     var templateData = null;
     pageContext['cosmia-template-data'] = {};
     //Iterate up the layout tree.
-    //Child layouts override parent layout data
+    //Child layout data overrides parent layout data
     do {
         templateData = handlebarsLayouts[layoutName]['cosmia-template-data'];
         pageContext['cosmia-template-data'] = Object.assign({}, (templateData ? templateData : {}), pageContext['cosmia-template-data']);
@@ -276,7 +299,7 @@ function _compilePages(outputDir, silent = false) {
         for (var p of keys(pageData)) {
             try {
                 var pageBody = _compilePage(pageData[p], {}, silent);
-                var outputPath = path.resolve(pageData[p].path.replace(pagesDir, outputDir) + '.html');
+                var outputPath = path.resolve(pageData[p].path.replace(pagesDir, outputDir).replace(collectionsDir, outputDir) + '.html');
 
                 //doing this stuff synchronously to avoid race conditions
                 mkdirp.sync(path.dirname(outputPath));
@@ -300,36 +323,34 @@ function _setupCosmia(srcFolder, silent = false, customData = {}) {
     pagesDir = path.resolve(srcFolder, COSMIA_PAGES_PATH);
     collectionsDir = path.resolve(srcFolder, COSMIA_COLLECTIONS_PATH);
 
-    return Promise.all(
-        _registerAppComponents() //,
-        //_processDirectory(pagesDir, EXTENSION_HBS, _processPage),
-        //_processDirectory(collectionsDir, EXTENSION_JSON, _processCollectionData) //collections overwrite pages
-    ).then(() => {
-        siteData = Object.assign({}, siteData, customData);
-        if (!silent) {
-            console.log(`${PACKAGE_NAME}: data extracted`);
-        }
-    }).catch((err) => {
-        console.log(err);
-    });
+    //this will create a race condition on pageData– when collections and pages conflict, behavior is undefined.
+    //DON'T SETUP CONFLICTING PATHS IN YOUR PAGES/COLLECTIONS (for now)
+    //TODO: implement handling for such race conditions
+    return Promise.all([
+        _registerAppComponents(),
+        _processDirectory(pagesDir, EXTENSION_HBS, _processPage),
+        _processDirectory(collectionsDir, EXTENSION_JSON, _processCollectionData)
+    ]);
 }
 
 function _setup(srcFolder, customData) {
-    return _setupCosmia(srcFolder, true, customData).catch((err) => {
-        console.error(chalk.red(err));
-    });
+    return _setupCosmia(srcFolder, true, customData);
 }
 
 function _compileSite(distFolder) {
-    return _compilePages(distFolder)
-        .catch((err) => {
-            console.error(chalk.red(err));
-        });
+    return _compilePages(distFolder);
 }
 
 function _cosmia(srcFolder, distFolder, customData = {}) {
-    _setup(srcFolder, customData).then(() => {
-        //_compileSite(distFolder);
+    Promise.resolve().then(() => {
+        return _setup(srcFolder, customData);
+    }).then(() => {
+        siteData = Object.assign({}, siteData, customData);
+        return;
+    }).then(() => {
+        return _compileSite(distFolder);
+    }).catch((err) => {
+        console.error(chalk.red(err));
     });
 }
 
---
+++ test/src/content/my-blog/second-post/index.md
@@ -0,0 +1,19 @@
+<script type="text/javascript" cosmia-collection-data>
+{
+    "published":"2017-02-03T15:56:14Z",
+    "updated":"2017-02-03T15:56:14Z",
+    "title": "second post!"
+}
+</script>
+
+<div cosmia-collection-content>
+# hey, another blog entry!
+
+renders to html
+
+</div>
+
+<div cosmia-collection-summary>
+short version
+
+</div>
