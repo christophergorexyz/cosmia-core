a5f59ee3c4b4a41a2725a4ffcdbad58348673034
Christopher J Gore <christopher.gore@razorfishhealth.com>
2017-02-14 15:45:58 -0500

    making sure text elements are trimmed to avoid extra paragraph tags

--- src/index.js
+++ src/index.js
@@ -77,6 +77,9 @@ function _extractCustomPageElement(page, attribute, process, tolerateChildren = 
         if (element.children.length) {
             try {
                 page[attribute] = process(element);
+                if(typeof page[attribute] === 'string'){
+                    page[attribute] = page[attribute].trim();
+                }
             } catch (err) {
                 throw page.path + '\n' + err;
             }
